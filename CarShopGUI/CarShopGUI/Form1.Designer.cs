75ae92a22b16b5544ff9a07af17e1d6862b0d5b4
JLAGCU <127072657+JLAGCU@users.noreply.github.com>
2023-05-14 20:59:22 -0700

    initial commit

---
+++ CarShopGUI/CarShopGUI/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace CarShopGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            listBox1 = new ListBox();
+            groupBox3 = new GroupBox();
+            listBox2 = new ListBox();
+            button2 = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            button3 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 212);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Create a Car:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(51, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Create Car";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(9, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Price:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(51, 74);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Model:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(51, 45);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(51, 16);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Make:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(listBox1);
+            groupBox2.Location = new Point(218, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 353);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Car Inventory";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(6, 22);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(188, 319);
+            listBox1.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(listBox2);
+            groupBox3.Location = new Point(523, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(200, 353);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Shopping Cart";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(6, 22);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(188, 319);
+            listBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(424, 174);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Add to Cart ->";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(529, 397);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Total Cost:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(608, 397);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 9;
+            label5.Text = "label5";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(583, 371);
+            button3.Name = "button3";
+            button3.Size = new Size(93, 23);
+            button3.TabIndex = 10;
+            button3.Text = "Checkout";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 109);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 15);
+            label6.TabIndex = 7;
+            label6.Text = "Year:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 135);
+            label7.Name = "label7";
+            label7.Size = new Size(39, 15);
+            label7.TabIndex = 8;
+            label7.Text = "Color:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(51, 103);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(51, 132);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(735, 418);
+            Controls.Add(button3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private ListBox listBox1;
+        private GroupBox groupBox3;
+        private ListBox listBox2;
+        private Button button2;
+        private Label label4;
+        private Label label5;
+        private Button button3;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label6;
+    }
+}
---
+++ CarShopGUI/CarShopGUI/Form1.cs
@@ -0,0 +1,71 @@
+namespace CarShopGUI
+{
+    public partial class Form1 : Form
+    {
+        Store store = new Store();
+
+        BindingSource carListBinding = new BindingSource();
+        BindingSource ShoppingListBinding = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetBindings();
+        }
+
+        private void SetBindings()
+        {
+            carListBinding.DataSource = store.CarList;
+            listBox1.DataSource = carListBinding;
+            listBox1.DisplayMember = "Display";
+            listBox1.ValueMember = "Display";
+
+            ShoppingListBinding.DataSource = store.ShoppingList;
+            listBox2.DataSource = ShoppingListBinding;
+            listBox2.DisplayMember = "Display";
+            listBox2.ValueMember = "Display";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Car newCar = new Car();
+            newCar.Make = textBox1.Text;
+            newCar.Model = textBox2.Text;
+            try
+            {
+                newCar.Price = Decimal.Parse(textBox3.Text);
+            }
+            catch (FormatException InvalidInput)
+            {
+                MessageBox.Show("Invalid input in Car Price. Please enter a number for price.");
+            }
+            try
+            {
+                newCar.Year = int.Parse(textBox4.Text);
+            }
+            catch (FormatException InvalidInput)
+            {
+
+                MessageBox.Show("Invalid input in Car Year. Please enter a number for year.");
+            }
+            newCar.Color = textBox5.Text;
+
+            store.CarList.Add(newCar);
+
+            carListBinding.ResetBindings(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            store.ShoppingList.Add((Car)listBox1.SelectedItem);
+
+            ShoppingListBinding.ResetBindings(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decimal total = store.checkout();
+            label5.Text = total.ToString();
+        }
+    }
+}
